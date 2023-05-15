df373024a0ba668bf2ffbb66aafd0a87ae46ebcd
hmmunioz <goticomananero@hotmail.com>
2023-05-15 03:18:01 -0300

    push

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,8 +3,6 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-
-  var handler: StreamHandlerSwift = StreamHandlerSwift()
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
@@ -12,6 +10,4 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
-    
 }
